c4fbf234967e27312410081486e0cea36cc0ee84
DevCyntrix <devcyntrix@hotmail.com>
2022-04-01 20:36:23 +0200

    Implement Holographic Displays

---
+++ src/main/java/de/helixdevs/deathchest/holographicdisplays/HDTextLine.java
@@ -0,0 +1,24 @@
+package de.helixdevs.deathchest.holographicdisplays;
+
+import com.gmail.filoghost.holographicdisplays.api.line.TextLine;
+import de.helixdevs.deathchest.api.hologram.IHologramTextLine;
+import org.jetbrains.annotations.NotNull;
+
+public class HDTextLine implements IHologramTextLine {
+
+    private final TextLine line;
+
+    public HDTextLine(TextLine line) {
+        this.line = line;
+    }
+
+    @Override
+    public void rename(@NotNull String text) {
+        line.setText(text);
+    }
+
+    @Override
+    public void remove() {
+        line.removeLine();
+    }
+}
