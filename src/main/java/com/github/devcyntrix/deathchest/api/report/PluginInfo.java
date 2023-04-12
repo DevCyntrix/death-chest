237585d4f4f0ff6bb4871e7f44d3c04aeb377c26
DevCyntrix <devcyntrix@hotmail.com>
2023-04-12 20:46:44 +0200

    Add extra map to the report and remove a comment from the config.yml

--- src/main/java/com/github/devcyntrix/deathchest/api/report/PluginInfo.java
+++ src/main/java/com/github/devcyntrix/deathchest/api/report/PluginInfo.java
@@ -1,18 +1,21 @@
 package com.github.devcyntrix.deathchest.api.report;
 
 import com.google.gson.annotations.SerializedName;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 
 import java.util.List;
 
-public record PluginInfo(@SerializedName("name") String name, @SerializedName("version") String version,
+public record PluginInfo(@SerializedName("name") String name,
+                         @SerializedName("version") String version,
+                         @SerializedName("enabled") boolean enabled,
                          @SerializedName("authors") List<String> authors,
                          @SerializedName("dependencies") List<String> dependencies) {
 
     public static PluginInfo of(Plugin plugin) {
         PluginDescriptionFile description = plugin.getDescription();
-        return new PluginInfo(description.getName(), description.getVersion(), description.getAuthors(), description.getDepend());
+        return new PluginInfo(description.getName(), description.getVersion(), Bukkit.getPluginManager().isPluginEnabled(plugin), description.getAuthors(), description.getDepend());
     }
 
 }
