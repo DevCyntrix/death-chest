c32c948553ccef5f1d7414b2bd4a886e00701c6e
DevCyntrix <devcyntrix@hotmail.com>
2023-08-03 11:48:45 +0200

    Fix chest migration

--- src/main/java/com/github/devcyntrix/deathchest/DeathChestPlugin.java
+++ src/main/java/com/github/devcyntrix/deathchest/DeathChestPlugin.java
@@ -34,7 +34,6 @@ import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
@@ -221,7 +220,7 @@ public class DeathChestPlugin extends JavaPlugin implements Listener, DeathChest
             debug(0, "Using death chest yaml storage");
             this.deathChestStorage = new YamlStorage();
             debug(0, "Initializing death chest storage...");
-            this.deathChestStorage.init(this, new MemoryConfiguration());
+            this.deathChestStorage.init(this, deathChestStorage.getDefaultOptions());
 
 
             this.deathChestController = new DeathChestController(this, getLogger(), this.auditManager, this.deathChestStorage);
