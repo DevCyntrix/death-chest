2a0ab6862fc5b82a5b08c9b2d237485684edb446
DevCyntrix <devcyntrix@hotmail.com>
2023-08-04 10:40:48 +0200

    Add hangar publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+
 plugins {
     `java-library`
     id("xyz.jpenilla.run-paper") version "2.1.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.papermc.hangar-publish-plugin") version "0.0.5"
 }
 
 group = "com.github.devcyntrix"
-version = "2.0.0"
+version = "2.0.0-rc1"
 
 repositories {
     mavenCentral()
@@ -79,5 +82,47 @@ tasks {
     }
     test {
         useJUnitPlatform()
+    }
+}
+
+hangarPublish {
+    publications.register("DeathChest") {
+        version.set(project.version as String)
+        namespace("CyntrixAlgorithm", "DeathChest")
+        changelog.set("https://github.com/DevCyntrix/death-chest/blob/main/CHANGELOG")
+
+        apiKey.set(System.getenv("API_KEY"))
+
+        if (!project.version.toString().contains('-')) {
+            channel.set("Release")
+        } else {
+            channel.set("Snapshot")
+        }
+
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.jar.flatMap { it.archiveFile })
+                platformVersions.set(listOf("1.17-1.20.1"))
+                dependencies.url("ProtocolLib", "https://www.spigotmc.org/resources/protocollib.1997/") {
+                    required.set(false)
+                }
+                dependencies.url("PlaceholderAPI", "https://www.spigotmc.org/resources/placeholderapi.6245/") {
+                    required.set(false)
+                }
+                dependencies.url("GriefPrevention", "https://www.spigotmc.org/resources/griefprevention.1884/") {
+                    required.set(false)
+                }
+                dependencies.url("RedProtect", "https://www.spigotmc.org/resources/redprotect-anti-grief-server-protection-region-management-1-7-1-19.15841/") {
+                    required.set(false)
+                }
+                dependencies.url("GriefDefender", "https://www.spigotmc.org/resources/1-12-2-1-19-4-griefdefender-claim-plugin-grief-prevention-protection.68900/") {
+                    required.set(false)
+                }
+                dependencies.url("WorldGuard", "https://dev.bukkit.org/projects/worldguard") {
+                    required.set(false)
+                }
+            }
+        }
+
     }
 }
